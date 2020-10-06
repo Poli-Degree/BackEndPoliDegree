f191e0a8aa4a3038c3dc6c74793f77c4f25b6d1d
Jhballen <Jhballen@poligran.edu.co>
2020-10-06 14:09:59 -0500

    despliegue heroku

--- PoliDegree/database.config/db.config.js
+++ PoliDegree/database.config/db.config.js
@@ -1,12 +1,12 @@
 
 
 //Heroku:
-//mysql://bf5bf61e6516e7:977e3aac@us-cdbr-east-02.cleardb.com/heroku_0bbbe33374fcc81?reconnect=true
+//mysql://bf5bf61e6516e7:@us-cdbr-east-02.cleardb.com/heroku_0bbbe33374fcc81?reconnect=true
 const dbconfig = {
     HOST: "us-cdbr-east-02.cleardb.com",
     USER: "bf5bf61e6516e7",
     PASSWORD: "977e3aac",
-    DB: "heroku_0bbbe33374fcc81heroku_0bbbe33374fcc81",
+    DB: "heroku_0bbbe33374fcc81",
     dialect: "mysql",
     pool: {
       max: 5,
