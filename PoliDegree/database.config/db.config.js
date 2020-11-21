a48930cc48f06f40f8b02acec99b85f059fd036a
Jhballen <Jhballen@poligran.edu.co>
2020-11-21 13:20:10 -0500

    Nueva db en heroku

--- PoliDegree/database.config/db.config.js
+++ PoliDegree/database.config/db.config.js
@@ -1,12 +1,8 @@
-
-
-//Heroku:
-//mysql://b9da3cec03769a:941608c7@us-cdbr-east-02.cleardb.com/heroku_5ad851fbeac6d0f?reconnect=true
 const dbconfig = {
-    HOST: "us-cdbr-east-02.cleardb.com",
-    USER: "b9da3cec03769a",
-    PASSWORD: "941608c7",
-    DB: "heroku_5ad851fbeac6d0f",
+    HOST: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "h1o8cqexemtcrx6o",
+    PASSWORD: "xs0wy92z64iiuo5n",
+    DB: "f3aw6gx7hfugk3v0",
     dialect: "mysql",
     pool: {
       max: 5,
