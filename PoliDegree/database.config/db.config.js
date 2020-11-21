0e86e67e9fbde5d52954ae96d27f967931193762
Jhballen <Jhballen@poligran.edu.co>
2020-11-21 13:23:30 -0500

    prueba db

--- PoliDegree/database.config/db.config.js
+++ PoliDegree/database.config/db.config.js
@@ -1,8 +1,8 @@
 const dbconfig = {
-    HOST: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    USER: "h1o8cqexemtcrx6o",
-    PASSWORD: "xs0wy92z64iiuo5n",
-    DB: "f3aw6gx7hfugk3v0",
+    HOST: "localhost",
+    USER: "jhballen",
+    PASSWORD: "jhon123",
+    DB: "polidegree",
     dialect: "mysql",
     pool: {
       max: 5,
